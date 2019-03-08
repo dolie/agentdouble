3438ff941000b6136345df03ae38d6a23304804f
dolie <ghafari.olivier@gmail.com>
2019-03-09 00:49:13 +0100

    V2

--- app.js
+++ app.js
@@ -4,41 +4,46 @@ var app = new Vue({
     el: '#app',
     data(){
         return {
-            playerMessage: '',
-            players: 3,
-            status: 'home',
-            waitingPlayer: 1,
-            timerDefault: 480,
-            timer: 0,
-            doubleAgent: 0,
+            playerMessage : '',
+            players       : 3,
+            status        : 'home',
+            waitingPlayer : 1,
+            timer         : 0,
+            timerEnd      : 0,
+            doubleAgent   : 0,
             defaultPlaces,
-            places : [],
-            place : '',
+            places        : [],
+            place         : '',
+            audio         : new Audio('alert.mp3')
         }
     },
     computed:{
         minutes(){
-            return Math.floor(this.timer / 60);
+            return Math.floor(this.timer / (60 *1000));
         },
         seconds(){
-            return this.timer % 60 < 10 ? `0${this.timer % 60}` : this.timer % 60;
+            let seconds = Math.floor(this.timer/1000)
+            return seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
         },
         alert(){
             return this.minutes <= 0 && this.status === 'started';
         },
         showGo(){
-            return this.minutes >= 7 ;
+            return this.minutes >= 7 && this.seconds > 50;
         }
     },
-    created(){
-        this.timer = this.timerDefault;
+    watch : {
+        status : function(newVal) {
+            newVal !== 'end' || this.audio.play();
+        }
     },
-    methods:{
+    methods : {
         start(){
             this.choosePlace();
             this.status = 'iddle';
             this.determiningAgent(this.players);
         },
+        //iddle waiting for click -> ready
         ready(){
             if ( this.doubleAgent === this.waitingPlayer){
                 this.playerMessage = 'Tu es l\'agent double !';
@@ -47,31 +52,29 @@ var app = new Vue({
             }
             this.status = 'ready';
         },
+        //ready waiting for click -> GO
         GO(){
             if ( this.waitingPlayer == this.players ){
                 this.status = 'started';
+                this.setTimer();
                 this.verifyTime();
             }else{
                 this.waitingPlayer++;
                 this.status = 'iddle';
             }
         },
+
         restart(){
             this.waitingPlayer = 1;
             this.doubleAgentfound = false;
-            this.timer = this.timerDefault;
             this.start();
         },
-        count(){
-            this.timer--;
-            this.verifyTime();
+        setTimer(){
+            this.timerEnd = Date.now() + 8 * 60 * 1000;
         },
         verifyTime(){
-            if (this.timer >= 0){
-                setTimeout(this.count,1000);
-            }else{
-                this.status = 'end';
-            }
+            this.timer = this.timerEnd - Date.now();
+            this.timer >= 0 ? setTimeout(this.verifyTime , 1000) : this.status = 'end';
         },
         determiningAgent(nbPlayers) {
             this.doubleAgent = Math.ceil(Math.random() * Math.floor(nbPlayers));
